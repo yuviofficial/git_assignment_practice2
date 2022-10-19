e9debee46fb22efa3b67d3b95b118bee68a6e403
Upuri07 <115460295+Upuri07@users.noreply.github.com>
2022-10-19 23:36:32 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,10 @@
 let str = "naman";
-for(i=0;i>=str.length;i--){
+var str2="";
+for(var i=str-1;i>=0;i--){
+
     str2+=str[i];
-}if(str=str2){
+}
+if(str==str2){
     console.log("it is not palindrome");
 }else{
     console.log("it is a palindrome");
